7f77a629166308254c5a835028b3985e4d966820
cllo5609 <lohrcl@oregonstate.edu>
2023-02-20 16:30:03 -0700

    Made 'check_pwd' function return False if input string doesn't contain a special character, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -2,6 +2,8 @@ def check_pwd(input_str):
     lower_check = False
     upper_check = False
     digit_check = False
+    special_check = False
+    special_chars = "~`!@#$%^&*()_+-="
     if len(input_str) < 8 or len(input_str) > 20:
         return False
 
@@ -20,7 +22,12 @@ def check_pwd(input_str):
             digit_check = True
             break
 
-    if upper_check and lower_check and digit_check:
+    for i in input_str:
+        if i in special_chars:
+            special_check = True
+            break
+
+    if upper_check and lower_check and digit_check and special_check:
         return True
 
     return False
