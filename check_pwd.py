b67089585cc5a2fa52be057b9b6b2d4806fbb90e
cllo5609 <lohrcl@oregonstate.edu>
2023-02-20 16:08:58 -0700

    Made 'check_pwd' function return True if input string contains at least one uppercase letter

--- check_pwd.py
+++ check_pwd.py
@@ -1,8 +1,20 @@
 def check_pwd(input_str):
+    lower_check = False
+    upper_check = False
     if len(input_str) < 8 or len(input_str) > 20:
         return False
 
     for i in input_str:
         if i.islower():
-            return True
+            lower_check = True
+            break
+
+    for i in input_str:
+        if i.isupper():
+            upper_check = True
+            break
+
+    if upper_check and lower_check:
+        return True
+
     return False
