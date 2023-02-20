8efe0f2ce28afc754a0ca63ce15846fb71443357
cllo5609 <lohrcl@oregonstate.edu>
2023-02-20 15:59:50 -0700

    Made 'check_pwd' function return True if input string contains at least one lowercase letter

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,8 @@
 def check_pwd(input_str):
     if len(input_str) < 8 or len(input_str) > 20:
         return False
-    return True
+
+    for i in input_str:
+        if i.islower():
+            return True
+    return False
