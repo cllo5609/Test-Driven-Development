316d710e2945b392ee85854d63ec738525437eff
cllo5609 <lohrcl@oregonstate.edu>
2023-02-20 16:13:03 -0700

    Wrote test_06 to see if a string without at least one digit is rejected correctly

--- tests.py
+++ tests.py
@@ -29,6 +29,10 @@ class TestCase(unittest.TestCase):
         input_str = '123435678u'
         self.assertFalse(check_pwd(input_str))
 
+    def test_06(self):
+        input_str = 'aBcDeFgHi'
+        self.assertFalse(check_pwd(input_str))
+
 
 if __name__ == '__main__':
     unittest.main()
