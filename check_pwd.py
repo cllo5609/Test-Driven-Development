f00e3702d614e134fe51d8363b773e0f9de173ca
cllo5609 <lohrcl@oregonstate.edu>
2023-02-20 16:53:12 -0700

    Final commit with completed tests.py and check_pwd.py files including header and comments

--- check_pwd.py
+++ check_pwd.py
@@ -1,32 +1,46 @@
+# Author     : Clinton Lohr
+# Date       : 02/17/2023
+# Course     : CS 362 - Software Engineering II
+# Assignment : TDD Hands On
+
+# Function for checking if a password is valid
 def check_pwd(input_str):
+
     lower_check = False
     upper_check = False
     digit_check = False
     special_check = False
     special_chars = "~`!@#$%^&*()_+-="
+
+    # Checks if the length of the entered password is between 8 and 20 characters (inclusive)
     if len(input_str) < 8 or len(input_str) > 20:
         return False
 
+    # Checks if the input string contains at least one lowercase letter
     for i in input_str:
         if i.islower():
             lower_check = True
             break
 
+    # Checks if the input string contains at least one uppercase letter
     for i in input_str:
         if i.isupper():
             upper_check = True
             break
 
+    # Checks if the input string contains at least one digit
     for i in input_str:
         if i.isdigit():
             digit_check = True
             break
 
+    # Checks if the input string contains at least one valid special character
     for i in input_str:
         if i in special_chars:
             special_check = True
             break
 
+    # Checks if all criteria for a valid password was met
     if upper_check and lower_check and digit_check and special_check:
         return True
 
--- tests.py
+++ tests.py
@@ -9,30 +9,37 @@ import unittest
 
 class TestCase(unittest.TestCase):
 
+    # Verifies if an empty string returns False
     def test_01(self):
         input_str = ''
         self.assertFalse(check_pwd(input_str))
 
+    # Verifies if a string of length less than 8 returns False
     def test_02(self):
         input_str = '1234567'
         self.assertFalse(check_pwd(input_str))
 
+    # Verifies if a string of length greater than 20 returns False
     def test_03(self):
         input_str = 'abcdefghijklmnopqrstu'
         self.assertFalse(check_pwd(input_str))
 
+    # Verifies if a string that does not contain at least one lowercase letter returns False
     def test_04(self):
         input_str = '123456789'
         self.assertFalse(check_pwd(input_str))
 
+    # Verifies if a string that does not contain at least one uppercase letter returns False
     def test_05(self):
         input_str = '123435678u'
         self.assertFalse(check_pwd(input_str))
 
+    # Verifies if a string that does not contain at least one digit returns False
     def test_06(self):
         input_str = 'aBcDeFgHi'
         self.assertFalse(check_pwd(input_str))
 
+    # Verifies if a string that does not contain at least one special character returns False
     def test_07(self):
         input_str = '1234aBcD'
         self.assertFalse(check_pwd(input_str))
